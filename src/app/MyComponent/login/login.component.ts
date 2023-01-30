28649b0dd3217cb6e753834605ee8d139c95fe0d
Pratik Patel <pratikpatel.p09@gmail.com>
2023-01-30 14:48:55 +0530

    final

--- src/app/MyComponent/login/login.component.ts
+++ src/app/MyComponent/login/login.component.ts
@@ -3,6 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { Component, NgModule, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponent } from 'src/app/app.component';
+import Swal from 'sweetalert2';
+
+
+
 
 @Component({
   selector: 'app-login',
@@ -13,6 +17,7 @@ import { AppComponent } from 'src/app/app.component';
 
 
 export class LoginComponent implements OnInit {
+  error = 'user name and password wrong!!'
   username!:string;
   password!:string;
   users = [
@@ -31,10 +36,13 @@ export class LoginComponent implements OnInit {
 
     if (this.username == 'admin' && this.password == 'admin' ) {
       console.log("correct");
+      Swal.fire("Good job!", "You are login!", "success");
       this.router.navigateByUrl('home');
     } else {
       console.log("somthing wrong");
+      Swal.fire("wrong","check username and password","error")
       this.router.navigateByUrl('login');
+
     }
   }
 }
